d5e447d72331251fdd4dd4cc1f68136738b06e0c
rajpatil1314 <168514582+rajpatil1314@users.noreply.github.com>
2024-09-27 12:12:30 +0530

    Add files via upload

---
+++ CORONA API/API/script.js
@@ -0,0 +1,17 @@
+// function show()
+// {
+//     fetch('https://api.rootnet.in/covid19-in/stats/latest')
+//     .then((res)=>res.json())
+//     .then((data)=>
+
+//         const regions = data.data.regional,
+//         tbody = document.getElementById('tableData'),
+//         console.log(regions)
+
+
+
+//     ).catch((err)=>console.log(err))
+// }
+// show();
+
+
